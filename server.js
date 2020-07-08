d000df9a3bd151dfc8bc2def625b4201fe776d4d
Gregory Monroe II <gregthemusician@gmail.com>
2020-07-07 22:21:16 -0400

    got server.js to have const variables, app.use and app.get routes set up

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs")
+const app = express();
+let db = require("./db/db.json")
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/",(req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get ("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/notes.html"));
+})
+
+
+
+
+app.get("/api/notes", (req, res) => {
+     fs.readFile('./db/db.json', (err, data) => {
+        if (err) throw err;
+        return data;
+      }).then(db => {
+          console.log('DB --->', db)
+          res.sendFile(data)
+      });
+})
+
+app.post("/api/notes", function(req, res) => {
+
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
