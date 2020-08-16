fdf935722eaffe9cc4d75025eb18a4deab80a23d
Gregory Monroe II <gregthemusician@gmail.com>
2020-08-16 13:45:29 -0400

    adding note works. deleting does not.

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,23 @@
 const router = require("express").Router();
+const db = require("../db/db.json");
 
 
-router.get("/api/notes", (req, res) => {
+router.get("/notes", (req, res) => {
+        return res.send(db);
+});
+
+router.post("/notes", (req, res) => {
+    const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+    };
+    console.log(newNote);
+    db.push(newNote);
+    res.json(newNote);
+});
+
+router.delete('/notes/:id', (req, res) => {
 
 });
 
-router.post("/api/notes", (req, res) => {
-
-});
-
-router.delete('/api/notes/:id', (req, res) => {
-});
+module.exports = router;
